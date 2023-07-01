04fd2b2fa5da9c627a20d1b28d7b165c15abe457
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-07-02 04:55:22 +0530

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,21 +4,5 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { -- further customize the options set by the community
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
   { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
 }
