9c7b1ad71d0c91be29feb0692d888d8e2d1dc911
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-07-02 03:10:46 +0530

    Update options.lua

--- options.lua
+++ options.lua
@@ -1,17 +1,4 @@
--- vim.api.nvim_create_autocmd("VimEnter", {
---   callback = function()
---     -- Only load the session if nvim was started with no args
---     if vim.fn.argc(-1) == 0 then
---       -- Save these to a different directory, so our manual sessions don't get polluted
---       resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
---     end
---   end,
--- })
--- vim.api.nvim_create_autocmd("VimLeavePre", {
---   callback = function() resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false }) end,
--- })
---
--- -- set vim options here (vim.<first_key>.<second_key> = value)
+-- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
     -- set to true or false etc.
