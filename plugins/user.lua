e3972c186b8c357ac84f0ba7d925f6bf238d8f31
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-07-02 04:55:43 +0530

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -21,4 +21,22 @@ return {
     "mg979/vim-visual-multi",
     event = "User AstroFile",
   },
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "InsertEnter" },
+    cmd = { "Copilot" },
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          dismiss = "<C-]>",
+        },
+      },
+    },
+  },
 }
