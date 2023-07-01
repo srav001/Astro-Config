97ce868643a8e84e793452302380f4109351705c
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-07-02 04:00:31 +0530

    Update null-ls.lua

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -4,30 +4,16 @@ return {
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
 
-    null_ls.builtins.formatting.prettier.with {
-      filetypes = {
-        "html",
-        "markdown",
-        "css",
-        "scss",
-        "vue",
-        "svelte",
-        "typescript",
-        "javascript",
-        "json",
-        "astro",
-        "typescriptreact",
-        "javascriptreact",
-      },
-    }
-
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettier.with {
+        filetypes = { "html", "markdown", "css", "scss", "vue", "svelte", "typescript", "javascript", "json", "astro",
+          "typescriptreact", "javascriptreact" },
+      },
     }
     return config -- return final config table
   end,
