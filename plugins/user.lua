41589ec847751368b9e449f97254c62dbb157619
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-07-03 13:18:04 +0530

    add options from nvchad

--- plugins/user.lua
+++ plugins/user.lua
@@ -39,4 +39,29 @@ return {
       },
     },
   },
+  {
+    "filipdutescu/renamer.nvim",
+    branch = "master",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "User AstroFile",
+    opts = {
+      title = 'Rename',
+      padding = {
+        top = 0,
+        left = 0,
+        bottom = 0,
+        right = 0,
+      },
+      min_width = 15,
+      max_width = 45,
+      border = true,
+      border_chars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+      show_refs = true,
+      with_qf_list = true,
+      with_popup = true,
+      handler = nil,
+    }
+  }
 }
