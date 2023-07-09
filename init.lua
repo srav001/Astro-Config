453e1b986579b1b8fd26cb0d01a34cfba7430f14
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-07-10 03:15:49 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -71,6 +71,10 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    vim.wo.wrap = true
+    vim.wo.linebreak = true
+    vim.wo.list = false
+
     local lspconfig = require "lspconfig"
 
     -- Eslint auto fix command
