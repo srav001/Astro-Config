ad4bed6058dd9bdf098b44d2c9ee7d9dabb11f8f
Sravan <sravansuresh1x@gmail.com>
2024-01-08 23:23:12 +0800

    fix: minor

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
       "html",
-      "scss",
+      "css",
       "markdown",
       "javascript",
       "typescript",
