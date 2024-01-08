f1d0a1ddad55aa798761cfd21866e982890fe920
Sravan <sravansuresh1x@gmail.com>
2024-01-09 00:47:05 +0800

    feat: try out catpuccin

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
 
-  colorscheme = "astrodark",
+  colorscheme = "catppuccin-mocha",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/core.lua
+++ plugins/core.lua
@@ -5,12 +5,6 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
         "    ███    ██ ██    ██ ██ ███    ███",
         "    ████   ██ ██    ██ ██ ████  ████",
         "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
@@ -20,13 +14,52 @@ return {
       return opts
     end,
   },
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      term_colors = true,
+      transparent_background = false,
+      color_overrides = {
+        mocha = {
+          rosewater = "#f4dbd6",
+          flamingo = "#f0c6c6",
+          pink = "#f5bde6",
+          mauve = "#c6a0f6",
+          red = "#ed8796",
+          maroon = "#ee99a0",
+          peach = "#f5a97f",
+          yellow = "#eed49f",
+          green = "#a6da95",
+          teal = "#8bd5ca",
+          sky = "#91d7e3",
+          sapphire = "#7dc4e4",
+          blue = "#8aadf4",
+          lavender = "#b7bdf8",
+          text = "#cad3f5",
+          subtext1 = "#b8c0e0",
+          subtext0 = "#a5adcb",
+          overlay2 = "#939ab7",
+          overlay1 = "#8087a2",
+          overlay0 = "#6e738d",
+          surface2 = "#5b6078",
+          surface1 = "#494d64",
+          surface0 = "#363a4f",
+          base = "#24273a",
+          mantle = "#1e2030",
+          crust = "#181926",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+        dropbar = {
+          enabled = true,
+          color_mode = true,
+        },
+      },
+    },
+  },
 }
